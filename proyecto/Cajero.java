b48273ee349b3e34057c97618042a1dc77cf89fd
wallyw998 <hgardi672mail.com>
2022-10-15 14:35:38 -0500

    Se agregan archivos

---
+++ proyecto/Cajero.java
@@ -0,0 +1,13 @@
+package cajero;
+import java.io.*;
+import java.util.Scanner;
+public class Cajero {
+    int intento;
+    int pw=2525;
+    int c;
+     //* @param args
+    public static void main(String[] args) {
+        System.out.println("Bienvenido\nInserte clave");
+    }
+    
+}
