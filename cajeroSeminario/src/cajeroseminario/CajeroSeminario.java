cb8e4155f225bd25ec819109128a45f0edf28efc
wallyw998 <115897425+wallyw998@users.noreply.github.com>
2022-11-02 17:36:08 -0600

    Update CajeroSeminario.java

--- cajeroSeminario/src/cajeroseminario/CajeroSeminario.java
+++ cajeroSeminario/src/cajeroseminario/CajeroSeminario.java
@@ -1,15 +1,18 @@
-package cajeroseminario;
+package cajero;
+import java.util.Date;
 import javax.swing.JOptionPane;
-public class CajeroSeminario {
+public class Cajero {
+
 public static void main(String[] args) {
-        int retiro,d=1000,Nip=1235,n,o,i,o2,o3,o4,o5;
+    Date date=new Date();
+        int retiro,d=1000,Nip=1235,n,o,i,o2;
         for(i=3;i>=1;i=i-1){
             n=Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese su NIP","Validacion",JOptionPane.QUESTION_MESSAGE));
             if(n==Nip){
                 for(i=3;i>=1;){
                     o=Integer.parseInt(JOptionPane.showInputDialog(null,"Seleccione opcion:\n1.-Saldo\n2.-Retiro\n3.-Historico\n4.-Salir","Menu Principal",JOptionPane.INFORMATION_MESSAGE));
                     switch(o){
-                        case 1:
+                        case 1 -> {
                             o2=Integer.parseInt(JOptionPane.showInputDialog(null,"Cuenta con:"+(d)+"\n1.-Volver\n2.-Salir","Menu Principal",JOptionPane.INFORMATION_MESSAGE));
                             if(d<0){
                                 JOptionPane.showMessageDialog(null,"Sin saldo\nVuelva pronto","Saldo",JOptionPane.INFORMATION_MESSAGE);
@@ -18,18 +21,18 @@ public static void main(String[] args) {
                             if(o2==2){
                                 System.exit(0);
                             }
-                            break;
-                        case 2:  
+                        }
+                        case 2 -> {  
                             retiro=Integer.parseInt(JOptionPane.showInputDialog(null,"Monto a retirar:","Retiro de efectivo",JOptionPane.QUESTION_MESSAGE));
                             if(retiro>d){
-                                o4=Integer.parseInt(JOptionPane.showInputDialog(null,"Imposible retirar\n Saldo insuficiente\n1.-Volver\n2.-Salir","Cantidad invalida",JOptionPane.ERROR_MESSAGE));
-                                if(o4==2){
+                                o2=Integer.parseInt(JOptionPane.showInputDialog(null,"Imposible retirar\n Saldo insuficiente\n1.-Volver\n2.-Salir","Cantidad invalida",JOptionPane.ERROR_MESSAGE));
+                                if(o2==2){
                                     System.exit(0);
                                 }
                             }
                             else{
-                                o3=Integer.parseInt(JOptionPane.showInputDialog(null,"Su retiro fue de $"+retiro+"\n1.-Volver\n2.-Salir","Retiro exitoso",JOptionPane.INFORMATION_MESSAGE));
-                                if(o3==2){
+                                o2=Integer.parseInt(JOptionPane.showInputDialog(null,"Su retiro fue de $"+retiro+"\n1.-Volver\n2.-Salir","Retiro exitoso",JOptionPane.INFORMATION_MESSAGE));
+                                if(o2==2){
                                     System.exit(0);
                                 }
                             }
@@ -38,19 +41,18 @@ public static void main(String[] args) {
                                 JOptionPane.showMessageDialog(null,"Sin saldo\nVuelva pronto","Saldo",JOptionPane.INFORMATION_MESSAGE);
                                 System.exit(0);
                             }
-                            break;
-                        case 3:
-                            o5=Integer.parseInt(JOptionPane.showInputDialog(null,"Se ha retirado: "+d+"\nfecha: "+fecha+"\n1.-Volver\n2.-Salir","Movimientos recientes",JOptionPane.INFORMATION_MESSAGE));
-                            if(o5==2){
+                        }
+                        case 3 -> {
+                            o2=Integer.parseInt(JOptionPane.showInputDialog(null,"Se ha retirado: "+d+"\nfecha: "+date+"\n1.-Volver\n2.-Salir","Movimientos recientes",JOptionPane.INFORMATION_MESSAGE));
+                            if(o2==2){
                                 System.exit(0);
                             }
-                            break;
-                        case 4:
+                        }
+                        case 4 -> {
                             JOptionPane.showMessageDialog(null,"Gracias por su visita","",JOptionPane.INFORMATION_MESSAGE);
                             System.exit(0);
-                            break;
-                        default:
-                            JOptionPane.showMessageDialog(null,"Opcion invalida","Error",JOptionPane.ERROR_MESSAGE);
+                        }
+                        default -> JOptionPane.showMessageDialog(null,"Opcion invalida","Error",JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
